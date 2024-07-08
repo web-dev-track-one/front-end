73d315dfba74cc1931227c257f2a915b73046e36
nsaroiu <22270559+nsaroiu@users.noreply.github.com>
2024-07-08 13:56:48 -0700

    Changed Announcements in LowerNav to Events.

--- front-end-app/src/components/NavBar/LowerNav/LowerNav.tsx
+++ front-end-app/src/components/NavBar/LowerNav/LowerNav.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import {useState} from "react";
 
 const pages = [
-  "Announcements",
+  "Events",
   "Due Dates",
   "Meet the Team",
   "Contact Us",
