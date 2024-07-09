4d585fef2519241ae2cbbcd56d871b89200f6797
Ivan <ivan.s.kraskov@gmail.com>
2024-07-09 15:48:57 -0400

    Events Page Completed v1.

--- front-end-app/src/MainBody.tsx
+++ front-end-app/src/MainBody.tsx
@@ -1,5 +1,6 @@
 import Home from "./components/Home";
 import Announcements from "./components/Announcements";
+import Events from "./components/Events";
 import { Route, Routes } from "react-router-dom";
 
 const MainBody = () => {
@@ -7,6 +8,7 @@ const MainBody = () => {
     <Routes>
       <Route element={<Home />} path="/Home" />
       <Route element={<Announcements />} path="/Announcements" />
+      <Route element={<Events />} path="/Events" />
     </Routes>
   );
 };
