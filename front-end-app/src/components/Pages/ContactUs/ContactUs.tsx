a1f41329a8608446e1650035300b95e82c986e1d
nsaroiu <22270559+nsaroiu@users.noreply.github.com>
2024-07-13 23:19:29 -0700

    Populated ContactUs Component with placeholder information.

--- front-end-app/src/components/Pages/ContactUs/ContactUs.tsx
+++ front-end-app/src/components/Pages/ContactUs/ContactUs.tsx
@@ -1,7 +1,16 @@
+import './ContactUs.css';
+
 const ContactUs = () => {
     return (
-        <>
-        </>
+        <div className={'contactus-container'}>
+            <div className={'card'}>
+                <h1>Contact Us</h1>
+                <p>
+                    If you have any questions or feedback, please feel free to contact us at <a
+                    href="mailto:johndoe@example.com">johndoe@example.com</a>.
+                </p>
+            </div>
+        </div>
     );
 }
 
