40b6e3911d4a88b9caab9c39c98da69fece8b934
nsaroiu <22270559+nsaroiu@users.noreply.github.com>
2024-07-22 23:51:11 -0700

    Cleaned up code format.

--- front-end-app/src/components/NavBar/LowerNav/LowerNav.tsx
+++ front-end-app/src/components/NavBar/LowerNav/LowerNav.tsx
@@ -1,61 +1,61 @@
 import AppBar from "@mui/material/AppBar";
-import { Box } from "@mui/material";
+import {Box} from "@mui/material";
 import styles from "../NavBar.module.css";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const pages = [
-  "Announcements",
-  "Events",
-  "Due Dates",
-  "Meet the Team",
-  "Contact Us",
-  "Login",
+    "Announcements",
+    "Events",
+    "Due Dates",
+    "Meet the Team",
+    "Contact Us",
+    "Login",
 ];
 
 const LowerNav = () => {
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-  const handleMouseEnter = (index: number) => {
-    setHoveredIndex(index);
-  };
+    const handleMouseEnter = (index: number) => {
+        setHoveredIndex(index);
+    };
 
-  const handleMouseLeave = () => {
-    setHoveredIndex(null);
-  };
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const page = (e.target as HTMLElement).innerText;
-    navigate(`/${page.replace(/\s+/g, "-").toLowerCase()}`); // Replaces spaces with hyphens
-  };
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        const page = (e.target as HTMLElement).innerText;
+        navigate(`/${page.replace(/\s+/g, "-").toLowerCase()}`); // Replaces spaces with hyphens
+    };
 
-  // #007FA3
+    // #007FA3
 
-  return (
-    <AppBar position="static">
-      <Box
-        sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-        className={styles.box}
-      >
-        {pages.map((page, index) => (
-          <Box
-            className={styles.link_boxes}
-            onClick={handleClick}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave()}
-            style={{
-              backgroundColor: hoveredIndex === index ? "#007FA3" : "#001E42",
-              transition: "background-color 0.2s ease",
-            }}
-          >
-            <p className={styles.link_text}>{page.toUpperCase()}</p>
-          </Box>
-        ))}
-      </Box>
-    </AppBar>
-  );
+    return (
+        <AppBar position="static">
+            <Box
+                sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}
+                className={styles.box}
+            >
+                {pages.map((page, index) => (
+                    <Box
+                        className={styles.link_boxes}
+                        onClick={handleClick}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave()}
+                        style={{
+                            backgroundColor: hoveredIndex === index ? "#007FA3" : "#001E42",
+                            transition: "background-color 0.2s ease",
+                        }}
+                    >
+                        <p className={styles.link_text}>{page.toUpperCase()}</p>
+                    </Box>
+                ))}
+            </Box>
+        </AppBar>
+    );
 };
 
 export default LowerNav;
--- front-end-app/src/components/Pages/Login/Login.tsx
+++ front-end-app/src/components/Pages/Login/Login.tsx
@@ -1,100 +1,101 @@
 // src/components/Login.js
-import React, { Dispatch, SetStateAction } from "react";
-import { useState, useEffect } from "react";
+import React, {Dispatch, SetStateAction} from "react";
+import {useState} from "react";
 import "./Login.css";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+
 interface LoginResponse {
-  token: string;
-  message?: string;
+    token: string;
+    message?: string;
 }
 
 interface LoginProps {
-  setAuthToken: Dispatch<SetStateAction<string | null>>;
+    setAuthToken: Dispatch<SetStateAction<string | null>>;
 }
 
-const Login = ({ setAuthToken }: LoginProps) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [showError, setShowError] = useState<string>("");
+const Login = ({setAuthToken}: LoginProps) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showError, setShowError] = useState<string>("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:3000/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+        try {
+            const response = await fetch("http://localhost:3000/auth/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, password}),
+            });
 
-      const data: LoginResponse = await response.json();
-      if (response.ok) {
-        // Save the token (e.g., in localStorage or a cookie)
-        localStorage.setItem("token", data.token);
-        setAuthToken(data.token);
+            const data: LoginResponse = await response.json();
+            if (response.ok) {
+                // Save the token (e.g., in localStorage or a cookie)
+                localStorage.setItem("token", data.token);
+                setAuthToken(data.token);
 
-        console.log("Login successful");
-        navigate("/admin");
-      } else {
-        if (response.status === 401) {
-          setShowError("Invalid credentials"); // Show invalid credentials error
-        } else {
-          setShowError("Server error"); // Show network error
+                console.log("Login successful");
+                navigate("/admin");
+            } else {
+                if (response.status === 401) {
+                    setShowError("Invalid credentials"); // Show invalid credentials error
+                } else {
+                    setShowError("Server error"); // Show network error
+                }
+            }
+        } catch (err) {
+            console.error(err);
+            setShowError("Network error"); // Show error message
         }
-      }
-    } catch (err) {
-      console.error(err);
-      setShowError("Network error"); // Show error message
-    }
-  };
+    };
 
-  return (
-    <div className="login-head">
-      <div className="login-container">
-        <div className="header-container">
-          <h1>Admin</h1>
+    return (
+        <div className="login-head">
+            <div className="login-container">
+                <div className="header-container">
+                    <h1>Admin</h1>
+                </div>
+                <div className="login-body">
+                    <form onSubmit={handleLogin}>
+                        <div className="login-group">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="username"
+                                className="login-control"
+                                id="email"
+                                aria-describedby="emailHelp"
+                                placeholder="username"
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="login-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                className="login-control"
+                                id="password"
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Log In
+                        </button>
+                    </form>
+                    {showError && (
+                        <div className="error-box">
+                            {showError}
+                            <button className="close-button" onClick={() => setShowError("")}>
+                                x
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-        <div className="login-body">
-          <form onSubmit={handleLogin}>
-            <div className="login-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="username"
-                className="login-control"
-                id="email"
-                aria-describedby="emailHelp"
-                placeholder="username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="login-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                className="login-control"
-                id="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Log In
-            </button>
-          </form>
-          {showError && (
-            <div className="error-box">
-              {showError}
-              <button className="close-button" onClick={() => setShowError("")}>
-                x
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
