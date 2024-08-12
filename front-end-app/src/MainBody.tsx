804dffc5e817668065c323ded10188703d8f652f
nsaroiu <22270559+nsaroiu@users.noreply.github.com>
2024-08-12 10:30:54 -0700

    Fixed footer to the bottom of the page. Accomplished by forcing the body to have a min-height of screen height - (navbar height + footer height).

--- front-end-app/src/MainBody.tsx
+++ front-end-app/src/MainBody.tsx
@@ -1,5 +1,5 @@
 import Home from "./components/Pages/Home/Home.tsx";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import ContactUs from "./components/Pages/ContactUs/ContactUs.tsx";
 import Login from "./components/Pages/Login/Login";
 import Admin from "./components/Pages/Admin/Admin";
@@ -8,27 +8,30 @@ import AnnouncementsPage from "./components/Pages/Announcements/AnnouncementsPag
 import EventsPage from "./components/Pages/Events/EventsPage.tsx";
 import MeetTheTeamPage from "./components/Pages/MeetTheTeam/MeetTheTeamPage.tsx";
 import DueDatesPage from "./components/Pages/DueDates/DueDatesPage.tsx";
+
 const MainBody = () => {
-  return (
-    <Routes>
-      <Route element={<Home />} path="/" />
-      <Route element={<AnnouncementsPage />} path="/announcements" />
-      <Route element={<EventsPage />} path="/events" />
-      <Route element={<DueDatesPage />} path="/due-dates" />
-      <Route element={<MeetTheTeamPage />} path="/meet-the-team" />
-      <Route element={<ContactUs />} path="/contact-us" />
-      <Route element={<Login />} path="/login" />
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute
-            token={localStorage.getItem("token")}
-            element={Admin}
-          />
-        }
-      />
-    </Routes>
-  );
+    return (
+        <div className={'main-body'}>
+            <Routes>
+                <Route element={<Home/>} path="/"/>
+                <Route element={<AnnouncementsPage/>} path="/announcements"/>
+                <Route element={<EventsPage/>} path="/events"/>
+                <Route element={<DueDatesPage/>} path="/due-dates"/>
+                <Route element={<MeetTheTeamPage/>} path="/meet-the-team"/>
+                <Route element={<ContactUs/>} path="/contact-us"/>
+                <Route element={<Login/>} path="/login"/>
+                <Route
+                    path="/admin"
+                    element={
+                        <ProtectedRoute
+                            token={localStorage.getItem("token")}
+                            element={Admin}
+                        />
+                    }
+                />
+            </Routes>
+        </div>
+    );
 };
 
 export default MainBody;
